4e1f1fc99b0cc893ca305dbb0cb5d2716b4e09b0
Rafał Sławik <rafal.slawik@gmail.com>
2014-10-08 18:57:56 +0200

    Events first test

---
+++ tests/test_Events.py
@@ -0,0 +1,10 @@
+import unittest
+
+from Events import Events
+
+class EventsTestCase(unittest.TestCase):
+	def test_createEvents(self):
+		events = Events()
+
+if __name__ == '__main__':
+	unittest.main()
