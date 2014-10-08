521cb411b55fa7ec03295605e63d49dcdd7d0fc7
Rafał Sławik <rafal.slawik@gmail.com>
2014-10-08 23:16:44 +0200

    simulation framework

--- simulate.py
+++ simulate.py
@@ -1,9 +1,16 @@
 import sys
 
-from Event import InjectEvent
+from Algorithm import Algorithm
+from Adversary import Adversary
+from Event import InjectEvent, SentEvent, ErrorEvent
 from Events import Events
 
 def play(algorithm, adversary, events):
+	def schedule(algorithm):
+		packet = algorithm.schedule()
+		if packet: events.schedule(SentEvent(time + packet, algorithm))
+		return packet
+
 	time = 0.0
 
 	while events.hasNext():
@@ -11,6 +18,19 @@ def play(algorithm, adversary, events):
 		time = event.time
 		print('>>', time, event)
 
+		algorithm.notify(event)
+		adversary.notify(event)
+
+		if not algorithm.sending:
+			packet = schedule(algorithm)
+			error = adversary.scheduleError(packet)
+			if error: events.schedule(ErrorEvent(time + error))
+
+		if not adversary.sending: schedule(adversary)
+
 # test play
 events = Events([InjectEvent(0.5, None), InjectEvent(1.0, None)])
-play(None, None, events)
+algorithm = Algorithm()
+adversary = Adversary()
+
+play(algorithm, adversary, events)
