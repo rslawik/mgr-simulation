215d572f74127be7523a060981b83226b50f422e
Rafał Sławik <rafal.slawik@gmail.com>
2014-10-08 23:00:03 +0200

    simulation framework

--- Algorithm.py
+++ Algorithm.py
@@ -1,5 +1,8 @@
 class Algorithm:
 	def __init__(self):
+		self.sending = None
+
+	def notify(self, event):
 		pass
 
 	def schedule(self):
@@ -7,3 +10,25 @@ class Algorithm:
 
 	def schedulePacket(self, packet):
 		pass
+
+# class SLPreamble(Algorithm):
+# 	preamble = []
+
+# 	def __init__(self, packet_lengths, gamma):
+# 		# call parent constructor
+# 		self.gamma = gamma
+
+# 	def schedule(self):
+# 		print 'SLPreamble schedule'
+# 		if not self.preamble and self.package_count(self.l_min) >= self.gamma:
+# 			self.preamble = [self.l_min] * self.gamma
+# 			for _ in range(self.gamma): del self.queue[self.queue.index(self.l_min)]
+# 		elif self.preamble:
+# 			self.last_package = self.preamble.pop()
+# 		elif self.l_max in self.queue:
+# 			self.last_package = self.l_max
+# 			del self.queue[self.queue.index(self.last_package)]
+# 		return self.last_package
+
+# 	def notify(self, status):
+# 		Algorithm.notify(self, status)
--- simulate.py
+++ simulate.py
@@ -1,3 +1,16 @@
 import sys
 
-print('simulation')
+from Event import InjectEvent
+from Events import Events
+
+def play(algorithm, adversary, events):
+	time = 0.0
+
+	while events.hasNext():
+		event = events.next()
+		time = event.time
+		print('>>', time, event)
+
+# test play
+events = Events([InjectEvent(0.5, None), InjectEvent(1.0, None)])
+play(None, None, events)
