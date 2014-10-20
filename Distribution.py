eb349e754ab9dd2f18e401657d11e453bb89b0cf
Rafał Sławik <rafal.slawik@gmail.com>
2014-10-20 17:24:50 +0200

    Distribution class

--- Distribution.py
+++ Distribution.py
@@ -1,3 +1,12 @@
 class Distribution:
 	def __init__(self, rate, distribution):
+		self.rate = rate
+		if sum(distribution.values()) != 1.0:
+			raise ValueError("{} is not a valid distribution".format(distribution))
+		self.distribution = distribution
+
+	def packet(self, packet):
+		return self.distribution[packet]
+
+	def packets(self):
 		pass
--- tests/test_Distribution.py
+++ tests/test_Distribution.py
@@ -2,6 +2,12 @@ import unittest
 
 from Distribution import Distribution
 
+class InvalidDistributionTestCase(unittest.TestCase):
+	def test_createInvalidDistribution(self):
+		with self.assertRaises(ValueError):
+			rate, dist = 3, {1: 0.5, 2: 0.25}
+			Distribution(rate, dist)
+
 class DistributionTestCase(unittest.TestCase):
 	def test_createDistribution(self):
 		rate, dist = 3, {1: 0.5, 2: 0.5}
@@ -10,6 +16,8 @@ class DistributionTestCase(unittest.TestCase):
 		self.assertEqual(distribution.packet(1), 0.5)
 		self.assertEqual(distribution.packet(2), 0.5)
 
+	def test_getPacktes(self):
+		pass
 
 if __name__ == '__main__':
 	unittest.main()
