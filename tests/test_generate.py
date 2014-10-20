a8ab2f55597cde8e7f9aa2919ddc442c83345bc5
Rafał Sławik <rafal.slawik@gmail.com>
2014-10-20 18:11:49 +0200

    generate

---
+++ tests/test_generate.py
@@ -0,0 +1,17 @@
+import unittest
+
+class GenerateTestCase(unittest.TestCase):
+	def test_exampleGenerate(self):
+		packets = {3.0: 0, 5.0: 0, 7.0: 0}
+		with open("tests/example/generate.out") as out:
+			for line in out:
+				_, packet = line.split()
+				packets[float(packet)] += 1
+		total = sum(packets.values())
+		# print(packets, total)
+		self.assertEquals(round(packets[3.0]/total, 2), 0.33)
+		self.assertEquals(round(packets[5.0]/total, 2), 0.17)
+		self.assertEquals(round(packets[7.0]/total, 2), 0.5) 
+
+if __name__ == '__main__':
+	unittest.main()
