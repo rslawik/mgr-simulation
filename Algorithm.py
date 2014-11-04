92b22775b16f2c9fdaae748cc6ac2a5f61a537d9
Rafał Sławik <rafal.slawik@gmail.com>
2014-11-04 01:03:29 +0100

    SLPreamble experiment

--- Algorithm.py
+++ Algorithm.py
@@ -47,24 +47,16 @@ class LLAlgorithm(Algorithm):
 			if self.queue[packet] > 0:
 				return self.schedulePacket(packet)
 
-# class SLPreamble(Algorithm):
-# 	preamble = []
+class SLPreamble(Algorithm):
+	def __init__(self, distribution, log):
+		super(SLPreamble, self).__init__(distribution, log)
+		self.preamble, self.gamma = [], int(self.distribution.packets[-1] / self.distribution.packets[0])
 
-# 	def __init__(self, packet_lengths, gamma):
-# 		# call parent constructor
-# 		self.gamma = gamma
-
-# 	def schedule(self):
-# 		print 'SLPreamble schedule'
-# 		if not self.preamble and self.package_count(self.l_min) >= self.gamma:
-# 			self.preamble = [self.l_min] * self.gamma
-# 			for _ in range(self.gamma): del self.queue[self.queue.index(self.l_min)]
-# 		elif self.preamble:
-# 			self.last_package = self.preamble.pop()
-# 		elif self.l_max in self.queue:
-# 			self.last_package = self.l_max
-# 			del self.queue[self.queue.index(self.last_package)]
-# 		return self.last_package
-
-# 	def notify(self, status):
-# 		Algorithm.notify(self, status)
+	def schedule(self):
+		if not self.preamble and self.queue[self.distribution.packets[0]] >= self.gamma:
+			self.preamble = [self.distribution.packets[0]] * self.gamma
+		elif self.preamble:
+			nextPacket = self.preamble.pop()
+			return self.schedulePacket(nextPacket)
+		elif self.queue[self.distribution.packets[-1]]:
+			return self.schedulePacket(self.distribution.packets[-1])
