9f8896bb6fb1a79af808bda0b43dceb7b4b9053f
Rafał Sławik <rafal.slawik@gmail.com>
2014-11-16 19:01:18 +0100

    Distribution changed to Model

--- tests/test_Model.py
+++ tests/test_Model.py
@@ -2,40 +2,35 @@ import unittest
 
 from Model import Model
 
-class InvalidModelTestCase(unittest.TestCase):
+class ModelTestCase(unittest.TestCase):
+	def test_createModel(self):
+		rate, distribution = 3, {1: 0.5, 2: 0.25, 1.5: 0.25}
+		model = Model(rate, distribution)
+		self.assertModelEqual(model, rate, distribution)
+
+	def assertModelEqual(self, model, rate, distribution):
+		packets = sorted(distribution.keys())
+
+		self.assertEqual(model.packets, packets)
+		self.assertEqual(model.rate, rate)
+		for packet in packets:
+			self.assertEqual(model.probability(packet), distribution[packet])
+
+class InvalidModelTestCase(ModelTestCase):
 	def test_createModel(self):
 		with self.assertRaises(AssertionError):
 			rate, distribution = 3, {1: 0.5, 2: 0.25}
 			model = Model(rate, distribution)
 
-class ModelTestCase(unittest.TestCase):
-	def setUp(self):
-		self.rate, self.distribution = 3, {1: 0.5, 2: 0.25, 1.5: 0.25}
-		self.model = Model(self.rate, self.distribution)
-
-	def test_createModel(self):
-		self.assertEqual(self.model.rate, self.rate)
-		self.assertEqual(self.model.probability(1), self.distribution[1])
-		self.assertEqual(self.model.probability(2), self.distribution[2])
-
-	def test_getPacktes(self):
-		self.assertEqual(self.model.packets, [1, 1.5, 2])
-
-class ModelFromFileTestCase(unittest.TestCase):
+class ModelFromFileTestCase(ModelTestCase):
 	def test_createModel_NoFile(self):
 		with self.assertRaises(FileNotFoundError):
 			model = Model.fromFile('tests/example/model.inXXX')
 
 	def test_createModel(self):
 		model = Model.fromFile('tests/example/model.in1')
-		self.assertEqual(model.packets, [3, 5, 7])
-		self.assertEqual(model.rate, 5)
-		self.assertEqual(model.probability(3.0), 0.33)
-		self.assertEqual(model.probability(5.0), 0.17)
-		self.assertEqual(model.probability(7.0), 0.5)
+		self.assertModelEqual(model, 5, {3.0: 0.33, 5.0: 0.17, 7.0: 0.5})
 
 	def test_createDistributionWithoutInfo(self):
 		model = Model.fromFile('tests/example/model.in2')
-		self.assertEqual(model.packets, [3, 5, 7])
-		self.assertEqual(model.rate, None)
-		self.assertEqual(model.probability(5.0), None)
+		self.assertModelEqual(model, None, {3.0: None, 5.0: None, 7.0: None})
