ea7fde3081a777f0eadca31b386660d7ea837f09
Rafał Sławik <rafal.slawik@gmail.com>
2014-11-19 23:38:19 +0100

    test cleanups

--- tests/TestPacketGenerator.py
+++ tests/TestPacketGenerator.py
@@ -1,21 +1,17 @@
 import unittest
 
 import PacketGenerator
-from Distribution import Distribution
+from Model import Model
 
 class PacketGeneratorTestCase(unittest.TestCase):
 	def test_experiment1iteration(self):
-		distribution = Distribution(None, {3: None, 5: None})
+		model = Model.withPackets(3, 5)
 		expected = [(0, 3), (0, 5), (8, 3), (8, 5), (16, 3), (16, 5)]
-		generator = PacketGenerator.experiment1(6, distribution)
+		generator = PacketGenerator.experiment1(6, model)
 		for ie, eic in zip(generator, expected):
 			self.assertEqual(ie, eic)
 
 	def test_experiment1length(self):
-		distribution = Distribution(None, {3: None, 5: None})
-		self.assertEqual(len(list(PacketGenerator.experiment1(6, distribution))), 6)
-		self.assertEqual(len(list(PacketGenerator.experiment1(5, distribution))), 5)
-
-
-if __name__ == '__main__':
-	unittest.main()
+		model = Model.withPackets(3, 5)
+		self.assertEqual(len(list(PacketGenerator.experiment1(6, model))), 6)
+		self.assertEqual(len(list(PacketGenerator.experiment1(5, model))), 5)
