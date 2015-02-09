71d392224ff4f18a8413da33e27ff283448bec75
Rafał Sławik <rafal.slawik@gmail.com>
2015-02-09 18:46:09 +0100

    througput.py

--- throughput.py
+++ throughput.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import sys
 
-from Logger import Reader
+from LogReader import LogReader
 
 if len(sys.argv) != 2:
 	print("Usage: {} <simulation.log>".format(sys.argv[0]))
 	sys.exit(1)
 
-reader = Reader(sys.argv[1])
+reader = LogReader(sys.argv[1])
 
-print(reader.ratios[-1])
+isSuccessful = lambda pe: pe.successful
+packet = lambda pe: pe.packet
+algSent = sum(map(packet, filter(isSuccessful, reader.algPackets)))
+advSent = sum(map(packet, filter(isSuccessful, reader.advPackets)))
+
+print(algSent / advSent)
