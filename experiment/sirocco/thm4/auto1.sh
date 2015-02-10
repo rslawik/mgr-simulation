e341e4277faa618b17c29cbadc6f166c00ee428e
Rafał Sławik <rafal.slawik@gmail.com>
2015-02-11 00:53:16 +0100

    sirocco:thm4 experiment

--- experiment/sirocco/thm4/auto1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../../../simulate.sh experiment/sirocco/thm4/model1.in stochastic $1 $2 Sirocco
--- experiment/sirocco/thm4/auto2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../../../simulate.sh experiment/sirocco/thm4/model2.in stochastic $1 $2 Sirocco
