39360339e75365b3b00d74e5307590556c39dae3
Rafał Sławik <rafal.slawik@gmail.com>
2015-02-10 22:14:13 +0100

    plot

--- plot.py
+++ plot.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 import sys
 from matplotlib import pyplot
+from itertools import accumulate
 
-from Logger import Reader
+from LogReader import LogReader
 
 if len(sys.argv) != 2:
 	print("Usage: {} <simulation.log>".format(sys.argv[0]))
 	sys.exit(1)
 
-reader = Reader(sys.argv[1])
+reader = LogReader(sys.argv[1])
 
-pyplot.plot(reader.times, reader.ratios)
+def calculateSent(packets):
+	sentPackets = list(filter(lambda pe: pe.successful, packets))
+	return list(zip(map(lambda pe: pe.end, sentPackets), accumulate(map(lambda pe: pe.packet, sentPackets))))
+
+algSent, advSent = calculateSent(reader.algPackets), calculateSent(reader.advPackets)
+
+times, ratios = [], []
+sAlg = 0.0
+for t, sAdv in advSent:
+	while algSent and algSent[0][0] <= t:
+		sAlg = algSent.pop(0)[1]
+	times.append(t)
+	ratios.append(sAlg / sAdv)
+
+pyplot.plot(times, ratios)
 pyplot.show()
